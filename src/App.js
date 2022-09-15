b71f7f8073a0d36de9549a28ddb3676e62c003fb
Rajesha T <rajesha@wittypen.com>
2022-09-15 08:58:54 +0530

    :rocket: Add features

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+import Data from './districts.json';
 
 function App() {
+  const [seach, setSearch] = useState('');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <center>
+      <h3>Enter Your District: </h3>
+      <input type="text" value={seach} onChange={(e) => setSearch(e.target.value)}/> <br />
+      {Data.filter(dist => dist.name.toLowerCase().includes(seach)).map(dist => {
+        return <div style={{"border":"1px solid black", "margin":"10px","padding":"10px", "maxWidth":"70%"}}>
+          {dist.name}
+        </div>
+      })}
+    </center>
   );
 }
 
 export default App;
+// style={{"border": "1px solid black", "margin" : "10px", "padding" : "10px", "maxWidth": "70%"}}
